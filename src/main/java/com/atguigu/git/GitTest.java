ef51eb4bca266abee7aa5be770da0d4bc23e0f77
Gao <Gao@atguigu.com>
2022-01-06 14:15:57 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 高章龙~
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+    }
+}
